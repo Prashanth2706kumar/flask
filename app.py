8226983fbb8c01b82f53b031ddd3598e5d727ce5
Prashanth2706kumar <kumarprashanth2706@gmail.com>
2025-06-24 22:04:11 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+app=Flask(__name__)
+CORS(app)
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    print("Received:", data)
+    return jsonify({"message": "Data received"}), 200
+if __name__ == "__main__":
+    app.run(port=5050,debug=False)
