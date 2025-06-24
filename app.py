ae1cb2a1aaa72634456e8808e2f5dab1448518ef
Prashanth2706kumar <kumarprashanth2706@gmail.com>
2025-06-24 22:17:29 +0100

    Update2

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
 app=Flask(__name__)
 CORS(app)
+
+port = int(os.environ.get("PORT", 5050)
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
     print("Received:", data)
     return jsonify({"message": "Data received"}), 200
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=5050,debug=False)
+    app.run(host="0.0.0.0",port=port,debug=False)
